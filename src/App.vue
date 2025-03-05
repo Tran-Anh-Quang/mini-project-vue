7949a1e97d6660f6ad4a25ac1a3df98484d2e448
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-03-05 15:01:28 +0700

    init project base

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <img src="@/assets/logo.svg" class="logo" width="60px" height="60px" alt="logo">
+    <nav>
+      <RouterLink to="/">
+        <div class="btn-main">Home</div>
+      </RouterLink>
+      <RouterLink to="/create">
+        <div class="btn-action">Create</div>
+      </RouterLink>
+    </nav>
+  </header>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  background-color: white;
+  align-items: center;
+  justify-content: space-between;
+  padding: .4rem .2rem;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.15);
+}
+
+nav {
+  display: flex;
+  column-gap: 1rem;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CreateView from "@/views/CreateView.vue";
+import ToDoDetailsView from "@/views/ToDoDetailsView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/create',
+      name: 'create',
+      // route level code-splitting
+      // this generates a separate chunk (Create.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: CreateView
+    },
+    {
+      path: '/todo/:id',
+      name: 'toDoDetails',
+      component: ToDoDetailsView
+    }
+  ],
+})
+
+export default router
